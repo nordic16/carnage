d92ac3dd6f11b9ba7ebeab9d2423ee3791cad428
Diogo <diogo6204@gmail.com>
2023-12-30 14:19:51 +0000

    removed profiles

--- db/migrate/20231230132422_create_profiles.rb
+++
@@ -1,8 +0,0 @@
-class CreateProfiles < ActiveRecord::Migration[7.1]
-  def change
-    create_table :profiles do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_30_140429) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_30_132422) do
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,12 +26,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_140429) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "workouts", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
