2eeccd8e21de4d0ef019cfbba905e0ae5f8c4d32
Diogo <diogo6204@gmail.com>
2024-01-01 13:32:25 +0000

    update works

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,11 +1,11 @@
 class WorkoutsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find
   def main
     @workouts = current_user.workouts
   end
 
   def destroy
-    @workout = Workout.find(params[:id])
     @workout.destroy()
     redirect_to workouts_path
   end
@@ -17,18 +17,28 @@ class WorkoutsController < ApplicationController
 
   def create
     @workout = current_user.workouts.new(workouts_params)
-    if @workout.save
-      redirect_to workout_path(@workout)
-    end
+    if @workout.save then redirect_to workout_path(@workout) end
   end
 
   def show
-    @workout = Workout.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @workout.update(workouts_params)
+      redirect_to workouts_path
+    end
   end
 
   def workouts_params
     params.require(:workout).permit(:title)
   end
 
+  def find
+    if params[:id]
+      @workout = Workout.find(params[:id])
+    end
+  end
 end
-
