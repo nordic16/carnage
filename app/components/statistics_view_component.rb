45f7e328c054704c147a97ed8c9dde118af0c2f8
Diogo <diogo6204@gmail.com>
2024-01-27 13:11:48 +0000

    fixed issues

--- app/components/statistics_view_component.rb
+++ app/components/statistics_view_component.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class StatisticsViewComponent < ViewComponent::Base
-  def initialize(topText:, midText:, bottomText:, bg:"bg-white", fg:"text-gray-100")
+  def initialize(topText:, midText:, bottomText:, bg:, fg:)
     @topText = topText
     @midText = midText
     @bottomText = bottomText
     @bg = bg
     @fg = fg
   end
-
 end
