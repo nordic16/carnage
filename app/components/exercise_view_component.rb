4a416d7c460904585098fadd3a1e3b141740a715
Diogo <diogo6204@gmail.com>
2024-02-09 12:46:19 +0000

    added exercise index view

--- app/components/exercise_view_component.rb
+++ app/components/exercise_view_component.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class ExerciseViewComponent < ViewComponent::Base
-  def initialize(title:, muscleGroup:, emphasis:)
+  def initialize(title:, muscleGroup:, emphasis:, bg_color:, fg_color:, bg_color_hover:)
     @title = title
     @muscleGroup = muscleGroup
     @emphasis = emphasis
+    @bg_color = bg_color
+    @fg_color = fg_color
+    @bg_color_hover = bg_color_hover
+
+    # color = bg_color[0..(bg_color.length - 5)]
+    # intensity = bg_color[(bg_color.length - 3)..(bg_color.length - 1)].to_i
+  
+    # @bg_color_hover = "#{color}-#{intensity - 150}"
+
   end
 
 end
--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,18 +1,22 @@
 class ExercisesController < ApplicationController
   before_action :authenticate_user!
 
-  def new() 
+  def new
     @exercise = Exercise.new
   end
 
-  def create() 
+  def create
     @exercise = Exercise.new(exercise_params)
     if @exercise.save
       redirect_to '/' 
     end
   end
 
-  def exercise_params() 
+  def exercise_params
     params.require(:exercise).permit(:name, :emphasis, :description, :exerciseType, muscle_group_ids: [])
+  end
+
+  def index
+    @exercises = Exercise.all
   end
 end
