566e8930b94d23415796c5210e0e160602b1ba91
Diogo <diogo6204@gmail.com>
2024-02-09 14:35:59 +0000

    asdadasd

--- app/components/exercise_view_component.rb
+++ app/components/exercise_view_component.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class ExerciseViewComponent < ViewComponent::Base
-  def initialize(title:, muscleGroup:, emphasis:, bg_color:, fg_color:, bg_color_hover:)
+  def initialize(title:, muscleGroup:, emphasis:)
     @title = title
     @muscleGroup = muscleGroup
     @emphasis = emphasis
-    @bg_color = bg_color
-    @fg_color = fg_color
-    @bg_color_hover = bg_color_hover
 
     # color = bg_color[0..(bg_color.length - 5)]
     # intensity = bg_color[(bg_color.length - 3)..(bg_color.length - 1)].to_i
