614adcfc44d37bcec5fda6fae4395f993da59812
Diogo <diogo6204@gmail.com>
2024-02-26 16:49:18 +0000

    validations

--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,4 +1,13 @@
 class ExerciseSet < ApplicationRecord
     belongs_to :exercise
     belongs_to :workout
+
+    validates :weight, :reps, :intensity, presence: true
+    
+    validates :weight, numericality: true
+    validates :reps, numericality: { only_integer: true }
+    validates :intensity, numericality: { in: 1..10 }
+
+    validates :weight, :reps, numericality: { greater_than_or_equal_to: 0 }
+
 end
