60b0547237d40d13d166385957df67e7b3a5d4e1
Diogo <diogo6204@gmail.com>
2024-03-12 11:15:41 +0000

    fixed what was possibly the stupidest bug ever.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ Exercise.create!([{
   name: "Wide Grip T-Bar Row",
   description: "Wide grip t-bar row",
   emphasis: "Traps, Rhomboids, Rear delts",
-  muscle_groups: [upper_back],
+  muscle_group_ids: [upper_back.id],
   user_id: 1,
   exerciseType: Exercise::WEIGHT_REPS
   },
